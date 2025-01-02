c066b934738601c1c89d82864051603b2cb2df4c
sAnter <egorsanter6@gmail.com>
2025-01-02 16:14:25 +0500

    test: For "users/views.py"
    Write tests for "users/views.py", set up LOGIN_REDIRECT_URL in
    settings.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 
 def logout_view(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         logout(request)
         return render(request, 'users/logged_out.html')
 
