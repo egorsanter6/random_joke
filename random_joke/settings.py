394c20d5d9f0ccf2203cfac348e7e7bed19fa5d2
sAnter <egorsanter6@gmail.com>
2025-01-23 20:23:12 +0500

    fix: Fix/rename files.

--- random_joke/settings.py
+++ random_joke/settings.py
@@ -25,12 +25,12 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY'),
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1'])
 
 
 # Application definition
